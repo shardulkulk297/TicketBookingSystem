929f0a38c20b8db920f4faa95b1a960b9fadb6cc
Shardul <shardulkulk297@gmail.com>
2025-03-20 17:06:28 +0530

    Started task 2

--- SQL/01_create_tables.sql
+++ SQL/01_create_tables.sql
@@ -5,6 +5,7 @@ create table Venue (
     venue_name varchar(25),
     address varchar(100)
 );
+
 create table Event (
     event_id int AUTO_INCREMENT PRIMARY KEY,
     event_date DATE,
@@ -21,7 +22,7 @@ create table Event (
 create table customer (
     customer_id int PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(20) UNIQUE NOT NULL,
-    phone_number int(10) UNIQUE NOT NULL
+    phone_number varchar(100) UNIQUE NOT NULL
 );
 create table booking (
     booking_id int PRIMARY KEY AUTO_INCREMENT,
@@ -37,3 +38,30 @@ Alter Table Event add CONSTRAINT FOREIGN KEY(booking_id) REFERENCES booking(book
 Alter table Customer add column customer_name varchar(255);
 Alter table Customer add column booking_id int;
 Alter table Customer add CONSTRAINT FOREIGN KEY(booking_id) REFERENCES booking(booking_id);
+
+
+ALTER TABLE Venue 
+MODIFY venue_name VARCHAR(25) NOT NULL,
+MODIFY address VARCHAR(100) NOT NULL;
+
+ALTER TABLE Event 
+MODIFY event_date DATE NOT NULL,
+MODIFY event_time TIME NOT NULL,
+MODIFY venue_id INT(10) NOT NULL,
+MODIFY total_seats INT(100) NOT NULL,
+MODIFY available_seats INT(100) NOT NULL,
+MODIFY ticket_price DECIMAL NOT NULL,
+MODIFY event_type ENUM('Movie', 'Sports', 'Concert') NOT NULL,
+MODIFY booking_id INT(10) NOT NULL;
+
+ALTER TABLE customer 
+MODIFY email VARCHAR(20) UNIQUE NOT NULL,
+MODIFY phone_number INT(10) UNIQUE NOT NULL;
+
+ALTER TABLE booking 
+MODIFY customer_id INT NOT NULL,
+MODIFY event_id INT NOT NULL,
+MODIFY num_tickets INT(50) NOT NULL,
+MODIFY total_cost INT(50) NOT NULL,
+MODIFY booking_date DATE NOT NULL;
+
